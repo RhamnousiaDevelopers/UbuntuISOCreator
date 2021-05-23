f26acfab9c921c7ed17593390bf31a127ad98c65
RhamnousiaDevelopers <84054273+RhamnousiaDevelopers@users.noreply.github.com>
2021-05-23 12:12:33 -0700

    Create iso_creator_support.py

---
+++ iso_creator_support.py
@@ -0,0 +1,38 @@
+import sys
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+
+def destroy_window():
+    # Function which closes the window.
+    global top_level
+    top_level.destroy()
+    top_level = None
+
+def update():
+    os.system("sudo apt update -y && sudo apt upgrade -y")
+
+if __name__ == '__main__':
+    import iso_creator
+    iso_creator.vp_start_gui()
+    update()
+
+
+
+
+
